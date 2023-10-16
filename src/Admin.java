1d38e5ca368c2057f47f9cdcc3b1060b2301b819
4910247Xro <ximenaortegam2605@gmail.com>
2023-10-16 16:41:26 -0500

    classe admi

--- src/Admin.java
+++ src/Admin.java
@@ -1,6 +1,20 @@
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.logging.Level;
 
 public class Admin extends User {
+    public static void crearEmpleado(Scanner scanner){
+        System.out.println("Agregando empleado");
 
+        System.out.println("Ingresa nombre");
+        String nombre = scanner.nextLine();
+
+        System.out.println("Ingresa rol");
+String rol = scanner.nextLine();
+
+        System.out.println("Fecha de contratacion");
+String fechaContratacion = scanner.nextLine();
+
+
+}
 }
