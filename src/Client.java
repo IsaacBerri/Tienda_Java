0d897f614e8bfad59d92c9cc36987d2f3a8b54a8
roberthcm <castilloroberth92@gmail.com>
2023-10-25 11:43:02 -0500

    Implementando metodo para agregar cliente a la hoja de exel

--- src/Client.java
+++ src/Client.java
@@ -1,15 +1,22 @@
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.logging.Level;
 
 public class Client extends User {
+    int id;
    String address;
     int contactNumber;
     int stratum;
 
+
     Client ()throws IOException{
         try {
             Database.newBasedate(); // Asegúrate de que el workbook esté inicializado
-            String[] titlesBasedate = {"Doc Id", "Name", "Address", "Contac Number", "Stratum"};
+            String[] titlesBasedate = {"ID", "Name", "Doc Id", "Address", "Contac Number", "Stratum"};
             Tools.createSheet("Client", titlesBasedate);
         } catch (IOException e) {
             // Maneja la excepción si ocurre un problema al inicializar el workbook
@@ -39,10 +46,32 @@ public class Client extends User {
 
         return stratum;
     }
-
     public void setRace(String race) {
 
         this.stratum = stratum;
     }
+    public void addClient(){
+        Row row = Database.sheet.createRow(Database.rows);
+        int ramdon = (int)Math.floor(Math.random()*1000);
+        id = ramdon;
+        for (int i = 0; i < Database.rows - 1; i++) {
+            String numString = String.valueOf(ramdon);
+            Row rowx = Database.sheet.getRow(i + 1);
+            Cell cell2 = rowx.getCell(0);
+            while (numString.equals(cell2.getStringCellValue())){
+                id = (int)Math.floor(Math.random()*1000);
+            }
+        }
+        Object[] newClient = {id, name, docId, address, contactNumber, stratum};
+        for (int i = 0; i <newClient.length; i++) {
+            Cell cell = row.createCell(i);
+            cell.setCellValue(newClient[i].toString());
+        }
 
+        try (FileOutputStream outputStream = new FileOutputStream("database.xlsx")) {
+            Database.workbook.write(outputStream);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
